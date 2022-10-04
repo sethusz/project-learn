75489d605b898bab37d14b2d7c5477db0d9ccb15
Oleksandr <oleksandrzelenjak@gmail.com>
2022-10-04 20:28:51 +0300

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+"use strict"; 
+
+// console.log('arr' + " - object");
+
+// console.log( 4 + " - object");
+
+let incr = 10,
+    decr = 10;
+
+
+console.log( 2 * 4 === '8');
+
+
+const isChecked = true;
+      isClose = false;
+
+    console.log( isChecked != isClose);
